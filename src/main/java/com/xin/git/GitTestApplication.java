8ba27e4b057d9a1849ca9eb7ba13c90307edcc54
username <email>
2022-04-15 17:13:37 +0800

    first commit

--- src/main/java/com/xin/git/GitTestApplication.java
+++ src/main/java/com/xin/git/GitTestApplication.java
@@ -11,6 +11,8 @@ public class GitTestApplication {
         System.out.println("hello ,get !");
         System.out.println("my hot_fix");
         System.out.println("master test!");
+        System.out.println("hot-fix test");
+        System.out.println("!!!!!!!h");
     }
 
 }
